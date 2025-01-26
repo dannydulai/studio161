4dadac42d9f0550d5be6d4f16c315cfefcb3476c
Danny Dulai <danny@roonlabs.com>
2025-01-26 16:20:59 -0500

    feat: Add interactive buttons with click and drag functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+//  await DesktopWindow.setWindowSize(Size(1000,40));
   runApp(const MyApp());
 }
 
@@ -13,34 +17,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Studio161',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const HomePage(),
-      builder: (context, child) {
-        return MediaQuery(
-          data: MediaQuery.of(context).copyWith(
-            size: const Size(640, 480),
-          ),
-          child: child!,
-        );
-      },
     );
   }
 }
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
   State<HomePage> createState() => _MyHomePageState();
 }
@@ -48,26 +33,79 @@ class HomePage extends StatefulWidget {
 class _MyHomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      body: Column(children: [
-        Row(
-          children: [
-            Text('Hello World'),
-            Text('Hello World'),
-          ],
-        ),
-        Row(
-          children: [
-            Text('Hello World'),
-          ],
-        ),
-      ]),
+      body: SizedBox(
+        width: 1000,
+        height: 80,
+        child: MainBar(),
+      ),
+    );
+  }
+}
+
+class MainBar extends StatelessWidget {
+  const MainBar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+      height: 80,
+      child: Row(
+        children: [
+          ClickButton(),
+          ClickDragButton(value: 0.4),
+          ClickDragButton(value: 0.8),
+        ],
+      ),
+    );
+  }
+}
+
+class ClickButton extends StatelessWidget {
+  const ClickButton({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: const Icon(Icons.home),
+      onTap: () {
+        print('click');
+      },
+    );
+  }
+}
+
+class ClickDragButton extends StatefulWidget {
+  const ClickDragButton({super.key, this.value = 0.0});
+  final double value;
+  @override
+  State<ClickDragButton> createState() => _ClickDragButtonState();
+}
+
+class _ClickDragButtonState extends State<ClickDragButton> {
+  late double value = widget.value;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   value = widget.value;
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: const Icon(Icons.home),
+      onVerticalDragStart: (details) {
+        print('drag-Start'); print(details);
+      },
+      onVerticalDragUpdate: (details) {
+        print('drag-Update'); print(details);
+      },
+      onVerticalDragEnd: (details) {
+        print('drag-end'); print(details);
+      },
+      onTap: () {
+        print('tap');
+      },
     );
   }
 }
