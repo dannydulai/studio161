d026604412e67f3d4133164f81b6a374c00d2fa5
Danny Dulai <danny@roonlabs.com>
2025-01-26 19:23:03 -0500

    refactor: Rewrite WingBridge with improved FFI and platform support

--- lib/wing_bridge.dart
+++ lib/wing_bridge.dart
@@ -1,67 +1,108 @@
-import 'dart:ffi' as ffi;
-import 'dart:io' show Platform;
-import 'package:path/path.dart' as path;
+// ignore_for_file: avoid_print
+
+import 'dart:ffi';
+import 'dart:isolate';
+import 'package:ffi/ffi.dart';
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
+
 import './wing_bindings.dart';
 
-class WingBridge {
-  static late final ffi.DynamicLibrary _lib;
-  static late final WingBindings _bindings;
-  static late final ffi.Pointer<WingConsole> _console;
-  
-  static void initialize() {
-    const libName = 'wing';
-    final libPrefix = Platform.isWindows ? '' : 'lib';
-    final libSuffix = Platform.isWindows ? '.dll' : (Platform.isMacOS ? '.dylib' : '.so');
-    final libFileName = '$libPrefix$libName$libSuffix';
-    
-    // Look for the library in the parent directory
-    final libraryPath = path.join('..', 'libwing', libFileName);
-    _lib = ffi.DynamicLibrary.open(libraryPath);
-    _bindings = WingBindings(_lib);
-  }
+class WingDiscover {
+    const WingDiscover({
+        required this.ip,
+        required this.name,
+        required this.model,
+        required this.serial,
+        required this.firmware
+    });
 
-  static List<WingDiscoveryInfo> discover({bool stopOnFirst = false}) {
-    final maxCount = 10;
-    final array = ffi.malloc<WingDiscoveryInfo>(maxCount);
-    try {
-      final count = _bindings.discover(array, ffi.sizeOf<ffi.Size>(), stopOnFirst ? ffi.Int32(1) : ffi.Int32(0));
-      return List.generate(count, (i) => array[i]);
-    } finally {
-      ffi.malloc.free(array);
+    final String ip;
+    final String name;
+    final String model;
+    final String serial;
+    final String firmware;
+
+    static List<WingDiscover> scan({bool stopOnFirst = false}) {
+        final ret = <WingDiscover>[];
+        final d = _bindings.discoverScan(stopOnFirst ? 1 : 0);
+        int i = 0;
+        int n = _bindings.discoverCount(d);
+        while (i < n) {
+            ret.add(WingDiscover(
+                        ip: _bindings.discoverGetIp(d, i).toDartString(),
+                        name: _bindings.discoverGetName(d, i).toDartString(),
+                        model: _bindings.discoverGetModel(d, i).toDartString(),
+                        serial: _bindings.discoverGetSerial(d, i).toDartString(),
+                        firmware: _bindings.discoverGetFirmware(d, i).toDartString() 
+                        ));
+            i++;
+        }
+
+        _bindings.discoverDestroy(d);
+        return ret;
     }
+}
+
+void _task(WingConsole model) {
+    _bindings.consoleRead(model._console);
+
+    print("flutter _task: done reading");
+}
+
+const _libname = 'wing';
+final DynamicLibrary _lib = switch (defaultTargetPlatform) {
+    TargetPlatform.android => DynamicLibrary.open("lib$_libname.so"),
+    TargetPlatform.linux => DynamicLibrary.open("lib$_libname.so"),
+    TargetPlatform.macOS => DynamicLibrary.open("lib$_libname.dylib"),
+    TargetPlatform.windows => DynamicLibrary.open("$_libname.dll"),
+    _ => DynamicLibrary.executable(),
+};
+
+final WingBindings _bindings = WingBindings(_lib);
+
+class WingConsole {
+  final Pointer<NativeWingConsole> _console;
+
+  static final Finalizer<WingConsole> _finalizer =
+      Finalizer((console) => console._close());
+
+  WingConsole._fromNative(this._console);
+
+  factory WingConsole.connect(String ip) {
+      final ipNative = ip.toNativeUtf8(allocator: malloc);
+      try {
+      var c = _bindings.consoleConnect(ipNative);
+          return WingConsole._fromNative(c);
+      } finally {
+          malloc.free(ipNative);
+      }
   }
 
-  static void connect(String ip) {
-    final ipNative = ip.toNativeUtf8(allocator: ffi.malloc);
-    try {
-      _console = _bindings.connect(ipNative);
-    } finally {
-      ffi.malloc.free(ipNative);
-    }
+  void _close() {
+  print("flutter _close");
+
+      _bindings.consoleDestroy(_console);
+      _finalizer.detach(this);
   }
 
-  static void destroy() {
-    _bindings.destroy(_console);
+  void read() {
+      Isolate.spawn<WingConsole>(_task, this);
   }
 
-  static void read() {
-    _bindings.read(_console);
+  void setString(int id, String value) {
+      final valueNative = value.toNativeUtf8(allocator: malloc);
+      try {
+          _bindings.consoleSetString(_console, id, valueNative);
+      } finally {
+          malloc.free(valueNative);
+      }
   }
 
-  static void setString(int id, String value) {
-    final valueNative = value.toNativeUtf8(allocator: ffi.malloc);
-    try {
-      _bindings.setString(_console, ffi.Uint32(id), valueNative);
-    } finally {
-      ffi.malloc.free(valueNative);
-    }
+  void setFloat(int id, double value) {
+      _bindings.consoleSetFloat(_console, id, value);
   }
 
-  static void setFloat(int id, double value) {
-    _bindings.setFloat(_console, ffi.Uint32(id), value);
-  }
-
-  static void setInt(int id, int value) {
-    _bindings.setInt(_console, ffi.Uint32(id), ffi.Int32(value));
+  void setInt(int id, int value) {
+      _bindings.consoleSetInt(_console, id, value);
   }
 }
